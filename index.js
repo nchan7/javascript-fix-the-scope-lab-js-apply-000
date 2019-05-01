5dae518d4dfdd4d16b5ca2ef00658620694f51fe
algebraic-animation-3644 <nathanchan1007@gmail.com>
2019-05-01 01:49:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var animal = 'dog'
+var animal = 'cat'
 
 function myAnimal() {
   return animal
 }
 
-function yourAnimal() {
+function yourAnimal(animal) {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
@@ -12,10 +12,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
